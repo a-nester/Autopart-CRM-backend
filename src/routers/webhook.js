b634077703962ee592bb397ff2f7c572cde19e30
a-nester <a_nester@ukr.net>
2024-10-22 11:45:51 +0300

    fix/auth-path

--- src/routers/webhook.js
+++ src/routers/webhook.js
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 import express from 'express';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
-import { webHookAuth } from '../controllers/auth';
+import { webHookAuth } from '../controllers/auth.js';
 
 export const router = Router();
 const jsonParser = express.json();
