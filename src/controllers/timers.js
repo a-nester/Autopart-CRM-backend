7881dabf90f086b8b54a9943a1ddf55ce0451e88
a-nester <a_nester@ukr.net>
2025-01-16 23:08:09 +0200

    fix/path

--- src/controllers/timers.js
+++ src/controllers/timers.js
@@ -1,4 +1,4 @@
-import { createDiscountTimer } from '../services/discountTimers';
+import { createDiscountTimer } from '../services/discountTimers.js';
 
 export const createTimerController = async (req, res) => {
   console.log(req.body);
--- src/services/discountTimers.js
+++ src/services/discountTimers.js
@@ -1,4 +1,4 @@
-import { DiscountTimerCollection } from '../db/models/DiscountTimer';
+import { DiscountTimerCollection } from '../db/models/DiscountTimer.js';
 
 export const createDiscountTimer = async (payload) => {
   const discountTimer = await DiscountTimerCollection.create(payload);
