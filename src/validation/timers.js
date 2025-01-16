80dbe587a05555184c3311812fd81323f105de1b
a-nester <a_nester@ukr.net>
2025-01-16 21:21:09 +0200

    fix/productId-validation

--- src/validation/timers.js
+++ src/validation/timers.js
@@ -2,5 +2,5 @@ import Joi from 'joi';
 
 export const createDiscountTimerSchema = Joi.object({
   shop: Joi.string().min(3).max(20).required(),
-  productId: Joi.number().max(10).required(),
+  productId: Joi.number().integer().min(0).max(9999999999).required(),
 });
