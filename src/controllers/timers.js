b498bc8ad2ba350972257e469d7d0386b53d99c9
a-nester <a_nester@ukr.net>
2025-01-16 21:36:29 +0200

    test/timers-controller

--- src/controllers/timers.js
+++ src/controllers/timers.js
@@ -1,3 +1,8 @@
 export const createTimerController = async (req, res) => {
   console.log(req.body);
+  res.status(200).json({
+    status: 200,
+    message: 'Already obtain body!',
+    data: req.body,
+  });
 };
