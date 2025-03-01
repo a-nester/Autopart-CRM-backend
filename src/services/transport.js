386b912a6a215d272b4c77fb0245bb7798914151
a-nester <a_nester@ukr.net>
2025-03-01 17:50:34 +0200

    fix/transport-sortby

--- src/services/transport.js
+++ src/services/transport.js
@@ -6,7 +6,7 @@ export const getAllTrips = async ({
   page = 1,
   perPage = 10,
   sortOrder = SORT_ORDER.ASC,
-  sortBy = 'unloadDate',
+  sortBy = 'loadDate',
   filter = {},
   company,
 }) => {
